de0c188df5f7d15bb2bee700943ac6483f9f425a
gabriel <soutogabriel04@gmail.com>
2024-04-08 11:33:37 -0300

    feat: improvements in cpf validation

--- bemobile-test/app/validators/client.ts
+++ bemobile-test/app/validators/client.ts
@@ -3,7 +3,7 @@ import vine from '@vinejs/vine'
 export const clientValidator = vine.compile(
   vine.object({
     name: vine.string().trim().minLength(4).maxLength(256),
-    cpf: vine.string().trim().maxLength(256),
+    cpf: vine.string().trim().fixedLength(11),
   })
 )
 
